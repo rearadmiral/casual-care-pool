b4cbbb82637924f282d2f9ad30552250b8e0ad11
Bill DePhillips <bill.dephillips@gmail.com>
2015-05-16 22:46:03 -0700

    sign up and enter profile info

--- casual-care-pool.js
+++ casual-care-pool.js
@@ -1,23 +1,34 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .aboutYou": function(event) {
+      try {
+        var name = event.target.name.value;
+        var mobileNumber = event.target.mobileNumber.value;
+        var address = event.target.address.value;
+        var result = Meteor.users.update(
+          {
+            _id: Meteor.userId()
+          },
+          {
+            $set: {
+              profile: {
+                name : name,
+                mobileNumber: mobileNumber,
+                address: address
+              }
+            }
+          }
+        );
+      } finally {
+        return false;
+      }
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
   });
 }
