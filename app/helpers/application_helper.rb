133bf36664e4615380bfe5d5e28c66dff08e662a
Bill DePhillips <bill.dephillips@gmail.com>
2015-06-06 22:27:49 -0700

    label support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
-
   def in_place_editable_field(model, attribute,
                               field_type: :text_field,
                               field_options: {},
                               label: nil)
+
     render partial: 'shared/in_place_editable_field',
                   locals: {
                     label: label || attribute.to_s.capitalize,
